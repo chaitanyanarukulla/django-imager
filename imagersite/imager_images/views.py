074962a40001dae48e5d23d51f383174bf7367c1
chai <chaitanyanarukulla@gmail.com>
2017-11-30 14:24:17 -0800

    updated library View

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -11,13 +11,31 @@ def library_view(request):
     if user == '':
         return redirect('home')
     photos = Photo.objects.filter(user__username=user)
+    photos_data = []
     for photo in photos:
         file, exe = os.path.splitext(photo.image.path)
         im = Image.open(photo.image.path)
-        im.thumbnail((250, 200))
+        im.thumbnail((250, 250))
         im.save(file + ".thumbnail", "JPEG")
+        photo_data = {}
+        photo_data['image'] = photo.image.path
+        photo_data['thumbnail'] = file + ".thumbnail"
+        photo_data['title'] = photo.title
+        photos_data.append(photo_data)
+    albums = Album.objects.filter(user__username=user)
+    albums_data = []
+    for album in albums:
+        file, exe = os.path.splitext(album.cover.image.path)
+        im = Image.open(album.cover.image.path)
+        im.thumbnail((250, 250))
+        im.save(file + ".thumbnail", "JPEG")
+        album_data = {}
+        album_data['thumbnail'] = file + ".thumbnail"
+        album_data['title'] = album.title
+        albums_data.append(album_data)
+
     context = {
-        'albums': [{}],
-        'photos': [{}]
+        'albums': albums_data,
+        'photos': photos_data
     }
     return render(request, 'imager_images/library.html', context)
