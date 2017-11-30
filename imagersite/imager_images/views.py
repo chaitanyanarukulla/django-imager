7a22763fd311bf318a1f26768bd0d8dedc0c725f
Megan Flood <mak.flood@comcast.net>
2017-11-29 22:48:51 -0800

    Fixed the library template Removed the attempt at using a custom tag and replaced with cycle.

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -12,11 +12,12 @@ def library_view(request):
         return redirect('home')
     photos = Photo.objects.filter(user__username=user)
     for photo in photos:
-        file, exe = os.path.splitext(photo.images.path)
+        file, exe = os.path.splitext(photo.image.path)
         im = Image.open(photo.image.path)
         im.thumbnail((250, 200))
         im.save(file + ".thumbnail", "JPEG")
     context = {
-
+        'albums': [{}],
+        'photos': [{}]
     }
     return render(request, 'imager_images/library.html', context)
