d1fbc3769eb9ca4ea8eb95d97f6ddd68571e7379
chai <chaitanyanarukulla@gmail.com>
2017-12-02 13:58:53 -0800

    Updated home ,library, photo and album views to classViews

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -2,9 +2,9 @@
 from django.conf.urls import url
 from imager_images import views
 urlpatterns = [
-    url(r'library$', views.library_view, name='library'),
-    url(r'photos$', views.photo_gallery_view, name='photo_gallery'),
-    url(r'albums$', views.album_gallery_view, name='album_gallery'),
-    url(r'photos/(?P<id>\d+)$', views.photo_detail_view, name='photo_detail'),
-    url(r'albums/(?P<id>\d+)$', views.album_detail_view, name='album_detail')
+    url(r'library$', views.LibraryView.as_view(), name='library'),
+    url(r'photos$', views.PhotoGalleryView.as_view(), name='photo_gallery'),
+    url(r'albums$', views.AlbumGalleryView.as_view(), name='album_gallery'),
+    url(r'photos/(?P<id>\d+)$', views.PhotoDetailView.as_view(), name='photo_detail'),
+    url(r'albums/(?P<id>\d+)$', views.AlbumDetailView.as_view(), name='album_detail')
 ]
--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,24 +1,24 @@
 """The main views for the Imager site."""
 from django.conf import settings
-from django.shortcuts import render
+from django.views.generic import TemplateView
 from imager_images.models import Photo
 
 
-def home_view(request):
-    """Get the Home page."""
-    if Photo.objects.filter(published='PUBLIC').count():
-        image = Photo.objects.filter(published='PUBLIC').order_by('?').first()
-        image_url = image.image.url
-        image_title = image.title
-    else:
-        image_url = settings.STATIC_URL + 'test_image.jpg'
-        image_title = 'High-Five'
+class HomeView(TemplateView):
+    """Render the home view template."""
 
-    return render(
-        request,
-        'imagersite/home.html',
-        {
-            'hero_img_url': image_url,
-            'hero_img_title': image_title
-        }
-    )
+    template_name = "imagersite/home.html"
+
+    def get_context_data(self):
+        """Get the data to send to the template as context."""
+        photos = Photo.objects.filter(published='PUBLIC')
+        if photos.count():
+            image = photos.order_by('?').first()
+            image_url = image.image.url
+            image_title = image.title
+        else:
+            image_url = settings.STATIC_URL + 'test_image.jpg'
+            image_title = 'High-Five'
+        return {'hero_img_url': image_url,
+                'hero_img_title': image_title
+                }
