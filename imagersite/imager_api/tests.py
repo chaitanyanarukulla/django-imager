86e83c440409e90e8292147f7d964891b3f4d3dc
Megan Flood <mak.flood@comcast.net>
2017-12-11 18:02:01 -0800

    Added tests for the API Added functional tests for the Photo API route to check for authentication and that it is getting the correct data.

--- imagersite/imager_api/tests.py
+++ imagersite/imager_api/tests.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.test import override_settings, RequestFactory, TestCase
+from django.urls import reverse_lazy
 from imager_images.tests import PhotoFactory
 from imager_profile.tests import UserFactory
 import factory
@@ -61,3 +62,76 @@ class PhotoAPIUnitTests(TestCase):
         view = PhotoListAPI(request=request)
         photos = view.get_queryset()
         self.assertEqual(photos.count(), self.user.photos.count())
+
+
+class PhotoAPIRouteTests(TestCase):
+    """Route tests for the imager_api app."""
+
+    @classmethod
+    @override_settings(MEDIA_ROOT=os.path.join(settings.BASE_DIR,
+                                               "test_media_for_photos_api_routes"))
+    def setUpClass(cls):
+        """Add one minimal user to the database."""
+        super(PhotoAPIRouteTests, cls).setUpClass()
+
+        os.system('mkdir {}'.format(
+            os.path.join(settings.BASE_DIR, 'test_media_for_photos_api_routes')
+        ))
+
+        user = UserFactory(username='bob')
+        user.set_password('password')
+        user.save()
+        cls.user = user
+
+        for _ in range(10):
+            photo = PhotoFactory(user=user)
+            photo.save()
+
+        user = UserFactory()
+        user.set_password(factory.Faker('password'))
+        user.save()
+        cls.second_user = user
+
+        for _ in range(5):
+            photo = PhotoFactory(user=user)
+            photo.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Remove the test directory."""
+        super(PhotoAPIRouteTests, cls).tearDownClass()
+        os.system('rm -rf {}'.format(
+            os.path.join(settings.BASE_DIR, 'test_media_for_photos_api_routes')))
+
+    def test_photos_api_route_not_logged_in_gets_403(self):
+        """Test photos API route gets a 403 status code if not logged in."""
+        response = self.client.get(reverse_lazy('api_photo_list'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_photos_api_route_logged_in_gets_200(self):
+        """Test photos API route gets a 200 status code if logged in."""
+        self.client.login(username='bob', password='password')
+        response = self.client.get(reverse_lazy('api_photo_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_photos_api_route_logged_in_gets_all_users_photos(self):
+        """Test photos API route gets all the user's photos if logged in."""
+        self.client.login(username='bob', password='password')
+        response = self.client.get(reverse_lazy('api_photo_list'))
+        self.assertEqual(len(response.json()), 10)
+
+    def test_photos_api_route_logged_in_gets_users_photos(self):
+        """Test photos API route gets the user's photos if logged in."""
+        self.client.login(username='bob', password='password')
+        response = self.client.get(reverse_lazy('api_photo_list'))
+        ids = [photo['id'] for photo in response.json()]
+        for photo in self.user.photos.all():
+            self.assertIn(photo.id, ids)
+
+    def test_photos_api_route_logged_in_gets_not_other_users_photos(self):
+        """Test photos API route does not get other user's photos if logged in."""
+        self.client.login(username='bob', password='password')
+        response = self.client.get(reverse_lazy('api_photo_list'))
+        ids = [photo['id'] for photo in response.json()]
+        for photo in self.second_user.photos.all():
+            self.assertNotIn(photo.id, ids)
